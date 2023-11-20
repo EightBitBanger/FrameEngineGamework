b7f80bf2f74799f2f84c76c7daffae35716b8ed2
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2023-11-19 17:57:13 -0700

    Changed directory name to dependencies

---
+++ dependencies/CodeBaseLibrary/types.h
@@ -0,0 +1,35 @@
+#ifndef TYPE_CONVERSION
+#define TYPE_CONVERSION
+
+//#define _WIN32_WINNT  0x500
+#include <SDKDDKVer.h>
+#include <windows.h>
+
+// Standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+
+#include <sstream>
+
+#include <string>
+
+/// Return a string containing the number in the float.
+__declspec(dllexport) std::string FloatToString(float value);
+
+/// Return a float containing the number in the string.
+__declspec(dllexport) float StringToFloat(std::string str);
+
+/// Return a string containing the number in the double.
+__declspec(dllexport) std::string DoubleToString(double value);
+
+/// Return a double containing the number in the string.
+__declspec(dllexport) double StringToDouble(std::string str);
+
+/// Return a string containing the number in the integer.
+__declspec(dllexport) std::string IntToString(int value);
+
+/// Return an integer containing the number in the string.
+__declspec(dllexport) int StringToInt(std::string str);
+
+#endif
