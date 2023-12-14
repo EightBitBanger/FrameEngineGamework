d3292857f56cd444c41b42bdb8fba1ca60e3c3a6
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2023-12-13 23:03:51 -0700

    Reworking networking

--- include/GameEngineFramework/Networking/NetworkSystem.h
+++ include/GameEngineFramework/Networking/NetworkSystem.h
@@ -1,7 +1,8 @@
 #ifndef _NETWORKING_SUPPORT__
 #define _NETWORKING_SUPPORT__
 
-#include <GameEngineFramework/Networking/ServerSocket.h>
+#include <GameEngineFramework/configuration.h>
+#include <WS2tcpip.h>
 
 #include <thread>
 #include <mutex>
@@ -15,55 +16,32 @@ class ENGINE_API NetworkSystem {
     
 public:
     
+    // Server
+    
+    /// Start hosting a server on a given port.
+    bool StartHost(unsigned int port);
+    
+    /// Stop hosting the server.
+    bool StopHost(void);
+    
     // Client
     
     /// Connect to a server at the given IP address and port.
-    bool ConnectToServer(std::string address, unsigned int port);
+    bool ConnectToHost(std::string ipAddress, unsigned int port);
     
-    /// Disconnect from the server.
-    bool DisconnectFromServer(void);
+    /// Disconnect from the host.
+    bool DisconnectFromHost(void);
     
-    /// Sends a message to the server.
-    void SendMessageToServer(char*, unsigned int size);
+    // State
     
-    /// Get a buffer string from the server. A blank string indicates no messages are available.
-    int GetMessageFromServer(char*, unsigned int size);
+    /// Return true if our socket is connected.
+    bool GetConnectionState(void);
     
-    // Server
+    /// Return true if we are hosting a server.
+    bool GetHostState(void);
     
-    /// Start a server listening for incoming connections on the given port.
-    bool StartServer(unsigned int port);
     
-    /// Stop the server no longer listening for incoming connections.
-    bool StopServer(void);
     
-    /// Send a message to a specific client at the given index.
-    bool Send(int index, char* message, unsigned int messageSz);
-    
-    // Active connections
-    
-    /// Check the connection state of the network.
-    bool GetConnectionStatus(void);
-    
-    /// Check if we are the server.
-    bool GetServerStatus(void);
-    
-    /// Get the number of hosts in the connections list.
-    unsigned int GetNumberOfConnections(void);
-    
-    /// Get a port by its index location in the connections list.
-    unsigned int GetPortByIndex(unsigned int index);
-    
-    /// Get a socket by its index location in the connections list.
-    SOCKET GetSocketByIndex(unsigned int index);
-    
-    /// Get a buffer string from a socket index location in the connections list.
-    std::string GetClientBufferByIndex(unsigned int index);
-    
-    /// Get a buffer string from a socket index location in the connections list.
-    void ClearClientBufferByIndex(unsigned int index);
-    
-    // Internal
     
     /// Initiate the network system.
     void Initiate(void);
@@ -71,29 +49,31 @@ public:
     /// Shutdown the network system.
     void Shutdown(void);
     
-    /// Update the network connections and timeouts.
+    /// Update network sockets.
     void Update(void);
     
     NetworkSystem();
     
-private:
     
-    // Host socket sub system.
-    SocketServer mHost;
     
-    // Networking thread
-    std::thread* networkSystemThread;
     
-    std::mutex mux;
-    
-    // State are we hosting or just a client
-    bool mIsHosting;
-    
-    // State are we currently connected
+    // State are we currently connected?
     bool mIsConnected;
     
-    // Buffer for incoming data
-    std::string mBuffer;
+    // State are we hosting a server?
+    bool mIsHosting;
+    
+    // Host listener or client socket, depending on the connection type
+    SOCKET mSocket;
+    
+    // Message buffer
+    char buffer[1024];
+    
+    // Client socket list
+    std::vector<SOCKET>       mSockets;
+    std::vector<std::string>  mMessages;
+    
+private:
     
 };
 
