6c94c5095ca4883a0d01bc87d17324948f734f03
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2023-12-26 20:45:38 -0700

    Added directional light type

--- include/GameEngineFramework/Renderer/components/light.h
+++ include/GameEngineFramework/Renderer/components/light.h
@@ -11,32 +11,37 @@ public:
     
     friend class RenderSystem;
     
+    /// State whether the light is active.
     bool isActive;
     
+    /// Type of light. Point or directional.
     short int type;
     
+    /// Distance beyond which the light will no longer be rendered.
     float renderDistance;
     
+    /// Position of the light in the scene.
     glm::vec3 position;
+    
+    /// Offset from the position where the light will be rendered.
     glm::vec3 offset;
+    
+    /// Direction toward which the light will shine. 
     glm::vec3 direction;
     
+    /// Color of the light.
     Color color;
     
+    /// Brightness multiplier.
     float intensity;
+    
+    /// Distance from the position beyond which the light will be cut off.
     float range;
+    
+    /// Lighting drop off multiplier.
     float attenuation;
     
     Light();
-    
-    
-    
-private:
-    
-    
-    
-    
-    
     
 };
 
--- include/GameEngineFramework/Renderer/enumerators.h
+++ include/GameEngineFramework/Renderer/enumerators.h
@@ -2,7 +2,8 @@
 // Render system enumerators
 
 // ============================================
-// Primitive drawing types
+// Mesh primitive drawing types
+
 #define  MESH_POINTS          GL_POINTS
 #define  MESH_LINES           GL_LINES
 #define  MESH_LINE_STRIP      GL_LINE_STRIP
@@ -16,8 +17,10 @@
 #define  MESH_QUAD_STRIP      GL_QUAD_STRIP
 #define  MESH_POLYGON         GL_POLYGON
 
+
 // ============================================
 // Material filtering
+
 #define  MATERIAL_FILTER_NONE              0
 #define  MATERIAL_FILTER_LINEAR            1
 #define  MATERIAL_FILTER_TRILINEAR         2
@@ -39,8 +42,10 @@
 #define  MATERIAL_WRAP_CLAMP               GL_CLAMP_TO_EDGE
 #define  MATERIAL_WRAP_CLAMP_BORDER        GL_CLAMP_TO_BORDER
 
+
 // ============================================
 // Blend functions
+
 #define  BLEND_ZERO                      GL_ZERO
 #define  BLEND_ONE                       GL_ONE
 #define  BLEND_SRC_COLOR                 GL_SRC_COLOR
@@ -63,8 +68,10 @@
 #define  BLEND_EQUATION_SUB              GL_FUNC_SUBTRACT
 #define  BLEND_EQUATION_REV_SUB          GL_FUNC_REVERSE_SUBTRACT
 
+
 // ============================================
-// Shader types
+// Shaders
+
 #define  SHADER_VERTEX                  GL_VERTEX_SHADER
 #define  SHADER_FRAGMENT                GL_FRAGMENT_SHADER
 //#define  SHADER_GEOMETRY                GL_GEOMETRY_SHADER
@@ -79,4 +86,12 @@
 // Winding
 #define  RENDER_WINDING_CW               GL_CW
 #define  RENDER_WINDING_CCW              GL_CCW
+
+
+// ============================================
+// Light types
+
+#define  LIGHT_TYPE_POINT                0
+#define  LIGHT_TYPE_DIRECTIONAL          1
+
 
