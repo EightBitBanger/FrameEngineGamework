1b036d234f4c80eb649fb74d97c47c67604a91b3
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-02-15 20:45:36 -0700

    Split off the engine extern include

--- src/Engine/EngineUpdate.cpp
+++ src/Engine/EngineUpdate.cpp
@@ -1,25 +1,4 @@
-#include <GameEngineFramework/Engine/Engine.h>
-
-ENGINE_API extern EngineComponents  Components;
-ENGINE_API extern ColorPreset       Colors;
-ENGINE_API extern NumberGeneration  Random;
-ENGINE_API extern StringType        String;
-ENGINE_API extern Logger            Log;
-ENGINE_API extern Timer             PhysicsTime;
-ENGINE_API extern Timer             Time;
-
-ENGINE_API extern Serialization     Serializer;
-ENGINE_API extern ResourceManager   Resources;
-ENGINE_API extern ScriptSystem      Scripting;
-ENGINE_API extern RenderSystem      Renderer;
-ENGINE_API extern PhysicsSystem     Physics;
-ENGINE_API extern AudioSystem       Audio;
-ENGINE_API extern InputSystem       Input;
-ENGINE_API extern MathCore          Math;
-ENGINE_API extern ActorSystem       AI;
-
-ENGINE_API extern PlatformLayer     Platform;
-
+#include <GameEngineFramework/Engine/EngineSystems.h>
 
 
 void EngineSystemManager::Update(void) {
