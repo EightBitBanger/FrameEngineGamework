19eb5bef4346b786a64ed06c0b5cc6aa7a542954
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-07-06 18:45:37 -0600

    Delete include directory

--- include/GameEngineFramework/ActorAI/neurons/NeuralLayer.cpp
+++
@@ -1,9 +0,0 @@
-#include <GameEngineFramework/ActorAI/neurons/NeuralLayer.h>
-
-
-NeuralLayer::NeuralLayer() {
-    
-    for (int i=0; i < NEURAL_LAYER_WIDTH; i++) node[i] = 1;
-    
-    return;
-}
