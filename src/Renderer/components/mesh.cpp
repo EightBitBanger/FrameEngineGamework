74d09d5767138ee6f910a680bbdd6cbde81e0ed1
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-07-08 20:58:08 -0600

    Building a chunk generator

--- src/Renderer/components/mesh.cpp
+++ src/Renderer/components/mesh.cpp
@@ -444,24 +444,11 @@ void Mesh::CalculateNormals(void) {
 
 void Mesh::SetNormals(glm::vec3 normals) {
     
-    for (unsigned int i=0; i < mVertexBufferSz; i += 3) {
+    for (unsigned int i=0; i < mVertexBufferSz; i++) {
         
-        glm::vec3 normal;
-        normal.x = normals.x;
-        normal.y = normals.y;
-        normal.z = normals.z;
-        
-        mVertexBuffer[i].nx   = normal.x;
-        mVertexBuffer[i].ny   = normal.y;
-        mVertexBuffer[i].nz   = normal.z;
-        
-        mVertexBuffer[i+1].nx = normal.x;
-        mVertexBuffer[i+1].ny = normal.y;
-        mVertexBuffer[i+1].nz = normal.z;
-        
-        mVertexBuffer[i+2].nx = normal.x;
-        mVertexBuffer[i+2].ny = normal.y;
-        mVertexBuffer[i+2].nz = normal.z;
+        mVertexBuffer[i].nx = normals.x;
+        mVertexBuffer[i].ny = normals.y;
+        mVertexBuffer[i].nz = normals.z;
         
         continue;
     }
