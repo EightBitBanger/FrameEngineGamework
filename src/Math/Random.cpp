4a7988e96cf6074b0802d5c0ac696b8c156a83fb
EightBitBanger <66331916+EightBitBanger@users.noreply.github.com>
2024-09-30 19:30:11 -0600

    Updating AI overall functionality

--- src/Math/Random.cpp
+++ src/Math/Random.cpp
@@ -10,24 +10,25 @@ NumberGeneration::NumberGeneration() {
     return;
 }
 
-void  NumberGeneration::Seed(int value) {
+void NumberGeneration::Seed(int value) {
+    
     srand(value);
-    stb_perlin_noise3_seed(0, 0, 0, 0, 0, 0, value);
+    
     return;
 }
 
-int  NumberGeneration::Range(int min, int max) {
+int NumberGeneration::Range(int min, int max) {
     return (rand() % max) + min;
 }
 
-float  NumberGeneration::Range(float min, float max) {
+float NumberGeneration::Range(float min, float max) {
     return ((float)(rand() % (int)(max * 100.0f)) * 0.01f) + min;
 }
 
-double  NumberGeneration::Range(double min, double max) {
+double NumberGeneration::Range(double min, double max) {
     return ((double)(rand() % (int)(max * (double)100.0)) * (double)0.01) + min;
 }
 
-float NumberGeneration::Perlin(float xcoord, float ycoord, float zcoord) {
-    return stb_perlin_noise3(xcoord, ycoord, zcoord, 0, 0, 0);
+float NumberGeneration::Perlin(float xcoord, float ycoord, float zcoord, int seed) {
+    return stb_perlin_noise3_seed(xcoord, ycoord, zcoord, 0, 0, 0, seed);
 }
